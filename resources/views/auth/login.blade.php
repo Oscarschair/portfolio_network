bc6e1f2ef8c017a1a305d21e210d0b91ff56e9a4
Oscarschair <chair1homan@gmail.com>
2021-12-11 00:09:31 +0900

    Merge branch 'header_change'

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,7 +41,7 @@
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" required>
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember">
                                     <label class="form-check-label" for="remember">
                                         {{ __('auth.remember Me') }}
                                     </label>
