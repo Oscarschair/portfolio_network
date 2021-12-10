c9df2b314fcedbda37ac2ed5244dd6dd9f4de937
Oscarschair <chair1homan@gmail.com>
2021-12-11 01:54:54 +0900

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,15 +79,19 @@
     <script type="text/javascript" src="{{ asset('bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('bootstrap-datepicker/locales/bootstrap-datepicker.ja.min.js') }}"></script> --}}
 
-
     <!-- Google Tag Manager -->
-    <!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WM8Z87Z');</script>-->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MPJ85VB');</script>
     <!-- End Google Tag Manager -->
 
 </head>
 <body>
     <!-- Google Tag Manager (noscript) -->
-    <!--<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WM8Z87Z" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>-->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPJ85VB"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
     <div id="wrapper">
