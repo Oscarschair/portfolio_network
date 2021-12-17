4d42704acda29deb4fa2dd8469b1d5467ca4d49f
Oscarschair <chair1homan@gmail.com>
2021-12-17 18:48:40 +0900

    htaccess update2

--- resources/views/myprofile.blade.php
+++ resources/views/myprofile.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 @inject('dateLib', 'App\Libs\DateLib')
-<link href="{{ asset('css/myprofile.css') }}" rel="stylesheet">
+<link href="{{ asset('css/myprofile.css') }}?<?php echo date('Ymd-Hi'); ?>" rel="stylesheet">
 <script src="{{ asset('js/filechecker.js') }}" defer></script>
 <div class="container">
     <div class="justify-content-center">
