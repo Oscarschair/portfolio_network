b2c41e0c69f1df34e37984dd994bc7cd79a1b940
oscarschair <chair1homan@gmail.com>
2023-02-15 13:28:11 +0900

    no message

--- resources/views/portfolio/view.blade.php
+++ resources/views/portfolio/view.blade.php
@@ -87,7 +87,7 @@
           </div>
           <div class="OSCSS-social-button">
             <div><a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button " data-show-count="false" data-text="{{$portfolio->title}} | ポートフォリオネットワーク">Tweet</a></div>
-            <div class="fb-share-button" data-href="https://portfolio-network.jp/portfolio/{{ $portfolio->id }}" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">シェア</a></div>
+            <div class="fb-share-button" data-href="https://portfolio-network.oscarchair.jp/portfolio/{{ $portfolio->id }}" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">シェア</a></div>
           </div>
         </div>
       </div>
