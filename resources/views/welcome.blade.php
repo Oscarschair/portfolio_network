98c990059b6a61342c1a9481b7d387cf33173d12
Oscarschair <contact@oscarchair.jp>
2026-09-04 22:23:36 +0900

    fix(ui): 「3つの柱」セクションのRECOMMENDEDバッジおよび特定カード強調を削除し、コアバリューとして3枚対等なカードUIに統一

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -147,8 +147,7 @@
             </div>
           </div>
 
-          <div class="OSCSS-columns-col pillar-card highlighted">
-            <div class="pillar-badge">RECOMMENDED</div>
+          <div class="OSCSS-columns-col pillar-card">
             <div class="pillar-icon-box icon-presentation">
               <svg width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M2 3h20M4 3v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3M10 9l5 3-5 3V9z"></path></svg>
             </div>
